df2498150aeb9c854560c341e66d92a5a13a741a
Paul Ghukasyan <paul.ghukasyan@gmail.com>
2019-05-09 14:44:03 +0300

    text changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <pre> ===>> I was changed! ===>> </pre>
+          <pre> ===>> I was changed! Feature branch! ===>> </pre>
         </p>
         <a
           className="App-link"
